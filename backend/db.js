37b8e19a6c2c670fa14a6707deb78b16ac4edb4d
nd974 <nicolas974dolphin@gmail.com>
2025-11-09 19:50:48 +0100

    test prod

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 import pg from "pg";
 import dotenv from "dotenv";
-dotenv.config();
+
+dotenv.config(); // charge les variables depuis .env
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 export const pool = new pg.Pool({
-    connectionString: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}.oregon-postgres.render.com/${DB_NAME}`,
+  connectionString: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}.oregon-postgres.render.com/${DB_NAME}`,
+  port: DB_PORT ? Number(DB_PORT) : 5432, // port par défaut PostgreSQL
 });
