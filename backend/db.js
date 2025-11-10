d60ca49a5d75d0ad06ccf5906d12a794ca687c40
nd974 <nicolas974dolphin@gmail.com>
2025-11-10 09:34:40 +0100

    opti ms

--- backend/db.js
+++ backend/db.js
@@ -8,5 +8,7 @@ export const pool = new Pool({
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.oregon-postgres.render.com/${process.env.DB_NAME}`,
   ssl: {
     rejectUnauthorized: false // nécessaire sur Render
-  }
+  },
+  idleTimeoutMillis: 30000, // garde la connexion ouverte 30 s
+  connectionTimeoutMillis: 5000
 });
--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://keskonmange-dx6caldf1-nd974s-projects.vercel.app",
+    "https://keskonmange-5db6ad3dc-nd974s-projects.vercel.app", //met pas le "/"
     "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
