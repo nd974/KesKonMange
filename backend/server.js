248afb8ec05710b4c9406b0d748dec294a5be6dd
nd974 <nicolas974dolphin@gmail.com>
2025-11-12 09:59:52 +0100

    block cold start

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,8 @@ app.use(cors({
   credentials: true
 }));
 
+
+
 app.use(express.json());
 
 
@@ -34,6 +36,11 @@ app.use("/profile", profileRoutes);
 app.use("/tag", tagRoutes);
 app.use("/recipe", recipeRoutes);
 app.use("/menu", menuRoutes);
+
+// Block cold start
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- frontend/ping.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    await fetch("https://keskonmange.onrender.com"); // ← ton endpoint Render
-    res.status(200).send("Ping sent");
-  } catch (err) {
-    console.error("Erreur ping backend:", err);
-    res.status(500).send("Erreur ping backend");
-  }
-}
