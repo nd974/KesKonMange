7b8a3f77c7b64cda84dfddfbafef0339e584e706
nd974 <nicolas974dolphin@gmail.com>
2025-11-12 19:09:25 +0100

    ping block cold start + Readme

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
-console.log("[DB_HOST]:", process.env.DB_HOST);
-console.log("[DB_USER]:", process.env.DB_USER);
-console.log("[DB_PASSWORD]:", process.env.DB_PASSWORD);
-console.log("[DB_NAME]:", process.env.DB_NAME);
-console.log("[DB_PORT]:", process.env.DB_PORT);
-
 import express from "express";
 import cors from "cors";
 import homeRoutes from "./routes/home.js";
@@ -26,8 +20,6 @@ app.use(cors({
   credentials: true
 }));
 
-
-
 app.use(express.json());
 
 
@@ -38,10 +30,6 @@ app.use("/recipe", recipeRoutes);
 app.use("/menu", menuRoutes);
 
 const PORT = process.env.PORT || 3000;
-
-
-
-
 
 app.get("/ping", (req, res) => {
   res.status(200).send("p");
