39c70f6cdb459c3e7c15baeed4c7ca09cc5e8598
nd974 <nicolas974dolphin@gmail.com>
2025-11-12 20:13:04 +0100

    readme maj

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://keskonmange-tan.vercel.app", //ne pas mettre le "/"
+    "https://keskonmange-tan.vercel.app",
     "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
